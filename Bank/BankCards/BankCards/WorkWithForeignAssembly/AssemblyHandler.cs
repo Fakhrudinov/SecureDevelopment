c3dfd4deb6c5a7ee3b9d146a57edc7d5fbb24350
Alexander Fakhrudinov <asbuka@gmail.com>
2022-02-25 08:59:37 +0300

    check handle dll placed in folder

--- Bank/BankCards/BankCards/WorkWithForeignAssembly/AssemblyHandler.cs
+++ Bank/BankCards/BankCards/WorkWithForeignAssembly/AssemblyHandler.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
-using StrongNameSignedLogonGenerator;
 
 namespace BankCards.WorkWithForeignAssembly
 {
@@ -9,17 +9,36 @@ namespace BankCards.WorkWithForeignAssembly
     {
         public static string LoadAssembly()
         {
-            DataGenerator dg = new DataGenerator();
-            string str = dg.GenerateSrting();
+            string current = Directory.GetCurrentDirectory();//E:\\Downloads\\Обучение\\C#\\DZ_Repozitoriy\\SecureDevelopment\\SecureDevelopment\\Bank\\BankCards\\BankCards
 
             var context = new CustomAssemblyLoadContext();
             // установка обработчика выгрузки
             context.Unloading += Context_Unloading;
+            // получаем путь к сборке MyApp
+            var assemblyPath = Path.Combine(Directory.GetCurrentDirectory(), "WorkWithForeignAssembly\\ForeignDLL\\StrongNameSignedLogonGenerator.dll");
+            // загружаем сборку
+            Assembly assembly = context.LoadFromAssemblyPath(assemblyPath);
+            // получаем тип
+            var type = assembly.GetType("StrongNameSignedLogonGenerator.DataGenerator");
+            // получаем его метод 
+            var greetMethod = type.GetMethod("GenerateSrting");
+
+            // вызываем метод
+            var instance = Activator.CreateInstance(type);
+            string result = (string)greetMethod.Invoke(instance, null);
+            // выводим результат метода на консоль
+            Console.WriteLine(result);
+
+            //// смотрим, какие сборки у нас загружены
+            //foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            //    Console.WriteLine(asm.GetName().Name);
 
             // выгружаем контекст
             context.Unload();
 
-            return str;
+
+
+            return result;
         }
         // обработчик выгрузки контекста
         private static void Context_Unloading(AssemblyLoadContext obj)
