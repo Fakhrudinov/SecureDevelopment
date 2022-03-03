97a7cb3e6bcb73ee6c44cf152c0b56f93d809612
Alexander Fakhrudinov <asbuka@gmail.com>
2022-03-03 07:32:10 +0300

    Fix to use staging in startUp, and set STAGING as default

--- Bank/BankCards/BankCards/Startup.cs
+++ Bank/BankCards/BankCards/Startup.cs
@@ -110,7 +110,7 @@ namespace BankCards
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
